ed81cc625cbcca4d8c1c7597e5fe63b976547e55
mahathu <m.hoffmann@systemli.org>
2022-05-15 02:28:58 +0200

    code for dccc

--- mdc3.py
+++ mdc3.py
@@ -1,40 +1,66 @@
+from curses import window
 import numpy as np
-# from numpy.polynomial.polynomial import Polynomial
+import pandas as pd
 import matplotlib.pyplot as plt
-import random
 
-def cov(X,Y):
-    # mean_X = sum(X)/len(X)
-    # mean_Y = sum(Y)/len(Y)
-    # return np.mean([
-    #     (x - mean_X)*(y - mean_Y) 
-    #     for x, y in zip(X, Y)
-    # ])
-    return np.cov(X,Y, ddof=0)[0][1]
+def cov(X: list[float],Y: list[float], bias: bool=True):
+    # bias: if True, the denominator will be n, otherwise (n - 1).
+    # see: https://math.stackexchange.com/q/2936143/
+    return np.cov(X,Y, bias)[0][1]
 
+def detrend(Y: list[float], deg: int=2) -> list[float]:
+    # this function assumes all data points 
+    # in the time series are equidistant.
+    coefficients = np.polyfit(np.arange(len(Y)), Y, deg)
+    trend = np.polyval(coefficients, np.arange(len(Y)))
 
-def detrend(X: list[float], Y: list[float], deg: int=2) -> list[float]:
-    coefficients = np.polyfit(X, Y, deg)
-    predicted = np.polyval(coefficients, X)
-    detrended = Y - predicted
-
-    fig, axes = plt.subplots(nrows=2, sharex=True)
-    axes[0].plot(X, Y, 'k')
-    axes[0].plot(X, predicted, 'b')
-    axes[0].set(title = f'Original series and {deg}. degree fit')
-
-    axes[1].plot(X, detrended, 'k')
-    axes[1].set(title = f'Detrended series')
-
-    plt.show()
+    return Y - trend
 
 
-def mdc3(X, Y, freq_min, freq_max, freq_step, sr, d):
+def MDC3(X: pd.Series, Y: pd.Series, freq_min: int, freq_max: int, freq_step: int, sr: int, d: int):
+    """ Calculate MDC3 correlation beteween two time series.
+    Arguments:
+        X: a time series
+        Y: another time series
+        freq_min: minimum frequency
+        freq_max: maximum frequency
+        freq_step: step size of the frequency rage
+        sr: sampling rate. Window size = sr/frequency
+        d: detrending degree
+    Returns:
+        MDC3 correlation between time series X and Y.
+    """ 
+
+    window_sizes = [sr/freq for freq in range(freq_min, freq_max+freq_step, freq_step)]
+    print(window_sizes)
     pass
 
 
-if __name__ == '__main__':
-    x = np.arange(50)
-    y = [y+random.uniform(-3,3) for y in np.linspace(22, 28, 50)]
+def DCCC(window_size: int, X: pd.Series, Y: pd.Series) -> float:
+    if len(X) != len(Y):
+        raise ValueError(f'time series must have same length ({len(X)} and {len(Y)} were given)')
 
-    detrend(x, y)
+    if len(X) % window_size > 0:
+        raise ValueError(
+            f'Time series of length {len(X)} not evenly divisible by window length {window_size}'
+        )
+    
+    # Detrend both series on a per-window scale:
+    detrended_x = [detrend(window) for window in np.split(X, len(X)/window_size)]
+    detrended_y = [detrend(window) for window in np.split(Y, len(Y)/window_size)]
+
+    return np.average(
+        [cov(window_x, window_y) for window_x, window_y in zip(detrended_x, detrended_y)]
+    ) / np.sqrt(
+        np.average([np.var(w) for w in detrended_x]) *
+        np.average([np.var(w) for w in detrended_y])
+    )
+
+if __name__ == '__main__':
+    # eeg = pd.read_excel('sub-hc7_ses-hc.xls', header=None)
+    eeg = pd.read_pickle('eegdata')
+    window_size = 512
+
+    # dccc = DCCC(window_size, X=eeg.iloc[:,0], Y=eeg.iloc[:,1])
+    # print(dccc)
+    mdc3 = MDC3(eeg.iloc[:,0], eeg.iloc[:,1], )
